4ed967c72da140a3c860d8af85a501b707b0e47b
Mert Tekmir <m.tekmir@newmotion.com>
2020-04-01 14:04:26 +0200

    input validation for inventory batch creation

--- src/main/scala/com/merit/api/inventoryCount/CreateInventoryCountBatch.scala
+++ src/main/scala/com/merit/api/inventoryCount/CreateInventoryCountBatch.scala
@@ -5,10 +5,16 @@ import api.JsonSupport
 import com.merit.modules.inventoryCount.InventoryCountService
 import akka.http.scaladsl.server.Route
 import api.CreateInventoryCountRequest
+import akka.http.scaladsl.model.StatusCodes
 
 object CreateInventoryCountBatch extends Directives with JsonSupport {
   def apply(inventoryCountService: InventoryCountService): Route =
     (pathEndOrSingleSlash & entity(as[CreateInventoryCountRequest])) { req =>
-      complete(inventoryCountService.create(req.name, req.brandId, req.categoryId))
+      (req.brandId, req.categoryId) match {
+        case (None, None) =>
+          complete(StatusCodes.BadRequest -> "Please choose either a category or a brand")
+        case _ =>
+          complete(inventoryCountService.create(req.name, req.brandId, req.categoryId))
+      }
     }
 }
